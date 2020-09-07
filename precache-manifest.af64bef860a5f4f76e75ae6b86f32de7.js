0705df387d65e4d94840e3542829295d135b85a5
kousenlsn <lsn_guilherme@hotmail.com>
2020-09-07 18:13:38 +0000

    Deploying to gh-pages from  @ e02890600c24ad1a1fb8fc1b7ea8fc053b94b95e 🚀

---
+++ precache-manifest.af64bef860a5f4f76e75ae6b86f32de7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56d6a52db70befc2acb684a2dbfd6dfe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e802292eb59c6b1dbb90",
+    "url": "/static/css/2.409ec117.chunk.css"
+  },
+  {
+    "revision": "cb08aa1bd7cd83b64dba",
+    "url": "/static/css/main.04c0ce95.chunk.css"
+  },
+  {
+    "revision": "e802292eb59c6b1dbb90",
+    "url": "/static/js/2.8538568a.chunk.js"
+  },
+  {
+    "revision": "ab639fcc7d5dc7ac39a140265e321a0b",
+    "url": "/static/js/2.8538568a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb08aa1bd7cd83b64dba",
+    "url": "/static/js/main.a2086931.chunk.js"
+  },
+  {
+    "revision": "a19fb50e2212c1e560d5",
+    "url": "/static/js/runtime-main.9143da59.js"
+  }
+]);
